f36f20553c1174ae697bc42da5300f3f760780b7
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-05-17 22:18:32 -0400

    ✨ chore: Remove unused SearchForm component and its styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
-<<<<<<< HEAD
-import Rotas from "./routes";
-=======
 import { Home } from "./pages";
->>>>>>> 42fe11b34544c630602e72658e20c63c6771d0a4
+import Rotas from "./routes";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
-
 
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-<<<<<<< HEAD
       <Router>
         <GlobalStyle />
         <Rotas />
       </Router>
-=======
       <GlobalStyle />
       <Home />
->>>>>>> 42fe11b34544c630602e72658e20c63c6771d0a4
-    </ThemeProvider>
+    </ThemeProvider >
   );
 }
--- src/pages/SearchForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, InputForm } from "./styles";
-
-export function SearchForm() {
-  return (
-    <Container>
-      <InputForm type="text" placeholder="Buscar conteúdo" />
-    </Container>
-  )
-}
